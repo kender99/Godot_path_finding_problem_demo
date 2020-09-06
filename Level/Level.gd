46d46ca619c04086d2755ed112ccd0fde6436a47
Christian <christian@ideaplant.com>
2020-09-06 18:42:58 +0200

    Added animated gif of problem

--- Level/Level.gd
+++ Level/Level.gd
@@ -2,30 +2,17 @@ extends Node2D
 
 var path : = PoolVector2Array()
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-						
 			path = $Navigation2D.get_simple_path($Player.position, event.position)
+			$Player.path = path			
 			$Line2D.points = path
-			print(path.size(), ' Path:',path, '  Player:', $Player.position, '  Target:', event.position)
-			#print(event.position)
-			update()
-			#$Player.path = path
+			print(path.size(), ' Path:',path, '  Player:', $Player.position, '  Target:', event.position)			
+			update()  # so line and circles get drawn
+			
 
 func _draw():
 	for p in path:
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 const speed = 200
-
+var path : = PoolVector2Array()
 
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
@@ -12,35 +12,29 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):		
 		velocity =Vector2(1,0)
 	if Input.is_action_pressed("ui_down"):
-		velocity= Vector2(0,1)
+		velocity= Vector2(0,1)	
 	
-	
-#	if Input.is_action_pressed("ui_left"):
-#		velocity.x -=1
-#	if Input.is_action_pressed("ui_up"):
-#		velocity.y -=1
-#	if Input.is_action_pressed("ui_right"):		
-#		velocity.x +=1
-#	if Input.is_action_pressed("ui_down"):
-#		velocity.y +=1
 	move_and_slide(velocity.normalized() * speed)
-	player_animation(velocity)
 
-
-func player_animation(velocity):
-	if velocity.y > 0:
-		$"AnimatedSprite".play("run")
-	elif velocity.y<0:
-		$"AnimatedSprite".play("run")
-	elif velocity.x !=0:
-		$"AnimatedSprite".play("run")
-		$"AnimatedSprite".flip_h = velocity.x > 0
-	else: 
-		$"AnimatedSprite".stop()
-		$"AnimatedSprite".frame = 0
-		
 	
-		
+	# Code to make the Player walk along the paths
+	# Calculate the movement distance for this frame
+	var distance_to_walk = speed * delta
+	
+	# Move the player along the path until he has run out of movement or the path ends.
+	while distance_to_walk > 0 and path.size() > 0:
+		var distance_to_next_point = position.distance_to(path[0])
+		if distance_to_walk <= distance_to_next_point:
+			# The player does not have enough movement left to get to the next point.
+			position += position.direction_to(path[0]) * distance_to_walk
+		else:
+			# The player get to the next point
+			position = path[0]
+			path.remove(0)
+		# Update the distance to walk
+		distance_to_walk -= distance_to_next_point
+
+
 		
 	
 	
